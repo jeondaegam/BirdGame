f886efc91e8e1959803f3183595bc6a7ebef84b4
yeoreum <jeondaegam@naver.com>
2024-09-07 05:32:44 +0900

    Add: 플레이어 입력받기
    - 플레이어 점프 기능 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float accel = 10f;
+    public float gravty = 10f;
+
+    public float currentSppeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSppeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 버튼을 누르고 있는 동안 올라가고
+        if (Input.GetButton("Jump"))
+        {
+            currentSppeed -= accel * Time.deltaTime;
+        }
+        // 떼는 순간 떨어진다
+        else
+        {
+            currentSppeed += gravty * Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.down * currentSppeed * Time.fixedDeltaTime);
+    }
+}
