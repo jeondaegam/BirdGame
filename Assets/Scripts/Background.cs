06ebe0fd286651ce27b6cbbe6d90618396ccfc98
yeoreum <jeondaegam@naver.com>
2024-09-07 16:24:21 +0900

    refactor: 배경 롤링 수정

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 5f;
     public float size = 19.2f;
     // Start is called before the first frame update
     void Start()
@@ -15,12 +15,13 @@ public class Background : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 좌측으로 배경사진을 이동 
-        transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+        // 좌측으로 이미지를 이동 
+        //transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
 
         if (transform.position.x <-size)
         {
-            // 배경사진의 위치를 변경한다 . 두 번째 이미지 다음 위치로  
+            // 이미지의 x position을 현재 위치에서 (size * 2)를 더한 지점에 배치한다.
             transform.Translate(new Vector3(size * 2, 0, 0));
         }
     }
